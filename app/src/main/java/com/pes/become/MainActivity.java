0572484e4ea2d709922bf32db06dd0245f9539aa
Júlia <julia.gomez.i@estudiantat.upc.edu>
2021-03-15 16:07:44 +0100

    Refactoring

--- app/src/main/java/com/pes/become/MainActivity.java
+++ app/src/main/java/com/pes/become/MainActivity.java
@@ -33,7 +33,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private int startHour, startMinute, endHour, endMinute;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,124 +43,124 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
             @Override
             public void onClick(View v) {
-                activitySheet = new BottomSheetDialog(MainActivity.this,R.style.BottomSheetTheme);
-                View sheetView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.activity_layout, findViewById(R.id.bottom_sheet));
+                createActivitySheet();
+            }
+        });
+    }
 
-                doneButton = sheetView.findViewById(R.id.doneButton);
-                cancelButton = sheetView.findViewById(R.id.cancelButton);
-                spinnerTheme = sheetView.findViewById(R.id.themeSpinner);
-                spinnerDay = sheetView.findViewById(R.id.daySpinner);
-                activityName = sheetView.findViewById(R.id.nameText);
-                activityDescr = sheetView.findViewById(R.id.descriptionText);
-                startTime = sheetView.findViewById(R.id.startTime);
-                endTime = sheetView.findViewById(R.id.endTime);
+    /*
+    * Funció per crear la pestanya de creació d'activitat
+    * Pre: ninguna
+    * Post: s'ha creat lapestanya de creació d'activitat
+    * */
+    private void createActivitySheet() {
+        activitySheet = new BottomSheetDialog(MainActivity.this,R.style.BottomSheetTheme);
+        View sheetView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.activity_layout, findViewById(R.id.bottom_sheet));
+
+        doneButton = sheetView.findViewById(R.id.doneButton);
+        cancelButton = sheetView.findViewById(R.id.cancelButton);
+        spinnerTheme = sheetView.findViewById(R.id.themeSpinner);
+        spinnerDay = sheetView.findViewById(R.id.daySpinner);
+        activityName = sheetView.findViewById(R.id.nameText);
+        activityDescr = sheetView.findViewById(R.id.descriptionText);
+        startTime = sheetView.findViewById(R.id.startTime);
+        endTime = sheetView.findViewById(R.id.endTime);
 
 
-                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(MainActivity.this, R.array.themesValues, android.R.layout.simple_spinner_item);
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                spinnerTheme.setAdapter(adapter);
-                spinnerTheme.setOnItemSelectedListener(MainActivity.this);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(MainActivity.this, R.array.themesValues, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerTheme.setAdapter(adapter);
+        spinnerTheme.setOnItemSelectedListener(MainActivity.this);
 
-                adapter = ArrayAdapter.createFromResource(MainActivity.this, R.array.dayValues, android.R.layout.simple_spinner_item);
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                spinnerDay.setAdapter(adapter);
-                spinnerDay.setOnItemSelectedListener(MainActivity.this);
+        adapter = ArrayAdapter.createFromResource(MainActivity.this, R.array.dayValues, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerDay.setAdapter(adapter);
+        spinnerDay.setOnItemSelectedListener(MainActivity.this);
 
 
-                doneButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        createActivity();
-                    }
-                });
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createActivity();
+            }
+        });
 
-                cancelButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        activitySheet.dismiss();
-                    }
-                });
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activitySheet.dismiss();
+            }
+        });
 
-                startTime.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        TimePickerDialog selectTime = new TimePickerDialog(
-                            MainActivity.this,
-                            android.R.style.Theme_Holo_Light_Dialog_MinWidth,
-                            new TimePickerDialog.OnTimeSetListener() {
-                                @Override
-                                public void onTimeSet(TimePicker view, int hour, int minute) {
-                                    startHour = hour;
-                                    startMinute = minute;
-                                    String time = startHour + ":" + startMinute;
-                                    SimpleDateFormat hformat = new SimpleDateFormat("HH:mm");
-                                    try {
-                                        Date date = hformat.parse(time);
-                                        startTime.setText(hformat.format(date));
-                                    } catch (ParseException e) {
-                                        e.printStackTrace();
-                                    }
+        startTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog selectTime = new TimePickerDialog(
+                    MainActivity.this,
+                    android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                    new TimePickerDialog.OnTimeSetListener() {
+                        @Override
+                        public void onTimeSet(TimePicker view, int hour, int minute) {
+                            startHour = hour;
+                            startMinute = minute;
+                            String time = startHour + ":" + startMinute;
+                            SimpleDateFormat hformat = new SimpleDateFormat("HH:mm");
+                            try {
+                                Date date = hformat.parse(time);
+                                startTime.setText(hformat.format(date));
+                            } catch (ParseException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }, 12, 0, true);
+                selectTime.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                selectTime.updateTime(startHour, startMinute);
+                selectTime.show();
+            }
+        });
+
+        endTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog selectTime = new TimePickerDialog(
+                        MainActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hour, int minute) {
+                                endHour = hour;
+                                endMinute = minute;
+                                String time = endHour + ":" + endMinute;
+                                SimpleDateFormat hformat = new SimpleDateFormat("HH:mm");
+                                try {
+                                    Date date = hformat.parse(time);
+                                    endTime.setText(hformat.format(date));
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
                                 }
-                            }, 12, 0, true);
-                        selectTime.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                        selectTime.updateTime(startHour, startMinute);
-                        selectTime.show();
-                    }
-                });
-
-                endTime.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        TimePickerDialog selectTime = new TimePickerDialog(
-                                MainActivity.this,
-                                android.R.style.Theme_Holo_Light_Dialog_MinWidth,
-                                new TimePickerDialog.OnTimeSetListener() {
-                                    @Override
-                                    public void onTimeSet(TimePicker view, int hour, int minute) {
-                                        endHour = hour;
-                                        endMinute = minute;
-                                        String time = endHour + ":" + endMinute;
-                                        SimpleDateFormat hformat = new SimpleDateFormat("HH:mm");
-                                        try {
-                                            Date date = hformat.parse(time);
-                                            endTime.setText(hformat.format(date));
-                                        } catch (ParseException e) {
-                                            e.printStackTrace();
-                                        }
-                                    }
-                                }, 12, 0, true);
-                        selectTime.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                        selectTime.updateTime(endHour, endMinute);
-                        selectTime.show();
-                    }
-                });
-
-
-
-                activitySheet.setContentView(sheetView);
-                activitySheet.show();
+                            }
+                        }, 12, 0, true);
+                selectTime.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                selectTime.updateTime(endHour, endMinute);
+                selectTime.show();
             }
         });
 
 
-
-
+        activitySheet.setContentView(sheetView);
+        activitySheet.show();
     }
 
     @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-    }
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) { }
 
     @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
+    public void onNothingSelected(AdapterView<?> parent) { }
 
     /*
     * Funció per crear una nova activitat i afegir-la a la rutina que està sent editada
     * Pre: ninguna
-    * Post: s'ha creat una nova activitat a la rutuina
+    * Post: s'ha creat una nova activitat a la rutina
     * */
     private void createActivity() {
         String name = activityName.getText().toString();
