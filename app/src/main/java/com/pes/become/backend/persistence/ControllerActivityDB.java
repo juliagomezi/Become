06eaa63deb5be2d6091660173891352b560eb47a
Ignasi <ignasi.de.jose@estudiantat.upc.edu>
2021-03-18 08:22:49 +0100

    Ampliat una mica el controladorActivity de la BD

--- app/src/main/java/com/pes/become/backend/persistence/ControllerActivityDB.java
+++ app/src/main/java/com/pes/become/backend/persistence/ControllerActivityDB.java
@@ -1,10 +1,37 @@
 package com.pes.become.backend.persistence;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ControllerActivityDB {
 
+    FirebaseFirestore db;
+    public ControllerActivityDB(){
+        db = FirebaseFirestore.getInstance();
+    }
 
 
-    public void createActivity(){}
+    /**
+     * Pre: La rutina de nom "routineName" ja existeix i l'activitat que es vol crear no
+     * se sol·lapa amb cap altre de la mateixa rutina.
+     * Param: "routineName" és el nom de la rutina ja existent.
+     * Param: "activityName" és el nom de l'activitat que es vol crear.
+     * Param: "theme" és el tema de l'activitat.
+     * Param: "actDescription" és la descripció del tema.
+     * Param: "actDay" és el dia on es vol col·locar l'activitat.
+     * Post:
+     */
+    public void createActivity(String routineName, String activityName, String actTheme,String actDescription, String actDay){
+        Map<String,Object> dataInput = new HashMap<>();
+        dataInput.put("name",activityName);
+        dataInput.put("theme",actTheme);
+        dataInput.put("description",actDescription);
+        dataInput.put("day",actDay);
+        db.collection(routineName).document().set(dataInput);
+
+    }
 
 
 
