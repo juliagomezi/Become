d247cc07c3eca1bd5a810ecd25163b08ac3fea5e
gerard-queralt <gerard.queralt@estudiantat.upc.edu>
2021-03-20 15:31:21 +0100

    documentacio DomainAdapterFactory

--- app/src/main/java/com/pes/become/backend/adapters/DomainAdapterFactory.java
+++ app/src/main/java/com/pes/become/backend/adapters/DomainAdapterFactory.java
@@ -9,12 +9,31 @@ import com.pes.become.backend.exceptions.InvalidTimeIntervalException;
 import com.pes.become.backend.exceptions.OverlappingActivitiesException;
 import com.pes.become.backend.persistence.ControllerPersistence;
 
+/**
+ * Classe que gestiona la comunicacio entre la capa de presentacio i la capa de domini, i la creacio dels adaptadors de cada classe de domini
+ */
 public class DomainAdapterFactory {
+    /**
+     * Unica instancia de la classe
+     */
     private static DomainAdapterFactory instance;
+    /**
+     * Unica instancia del controlador de persistencia
+     */
     private static ControllerPersistence controllerPersistence = new ControllerPersistence();
+    /**
+     * Unica instancia de l'adaptador de la classe Rutina
+     */
     private static RoutineAdapter routineAdapter;
+    /**
+     * Unica instancia de l'adaptador de la classe Activitat
+     */
     private static ActivityAdapter activityAdapter;
 
+    /**
+     * Metode per obtenir la instancia de la classe
+     * @return instancia
+     */
     public static DomainAdapterFactory getInstance(){
         if(instance == null){
             instance = new DomainAdapterFactory();
@@ -22,24 +41,52 @@ public class DomainAdapterFactory {
         return instance;
     }
 
+    /**
+     * Metode per obtenir l'adaptador de Rutina
+     */
     private static void getInstanceRoutineAdapter(){
         if(routineAdapter == null){
             routineAdapter = RoutineAdapter.getInstance();
         }
     }
 
+    /**
+     * Metode per obtenir l'adaptador de Activitat
+     */
     private static void getInstanceActivityAdapter(){
         if(activityAdapter == null){
             activityAdapter = ActivityAdapter.getInstance();
         }
     }
 
+    //Aqui necessitarem, mes endavant, el correu de l'usuari
+    /**
+     * Metode per crear una rutina
+     * @param name nom de la rutina
+     */
     public void createRoutine(String name){
         getInstanceRoutineAdapter();
         routineAdapter.createRoutine(name);
         controllerPersistence.createNewRoutine(name);
     }
 
+    /**
+     * Metode per crear una activitat
+     * @param routineName nom de la rutina de la que forma part l'activitat
+     * @param name nom de l'activitat
+     * @param description descripcio de l'activitat
+     * @param theme tema de l'activitat
+     * @param iniH hora d'inici de l'activitat
+     * @param iniM minut d'inici de l'activitat
+     * @param endH hora de fi de l'activitat
+     * @param endM minut de fi de l'activitat
+     * @param iniDayString dia d'inici de l'activitat
+     * @param endDayString dia de fi de l'activitat
+     * @throws InvalidTimeIntervalException es llença si el temps d'inici no es anterior al temps de fi
+     * @throws InvalidDayIntervalException es llença si el dia de fi es anterior al dia d'inici
+     * @throws InvalidTimeException es llença si les hores o minuts no tenen format valid
+     * @throws OverlappingActivitiesException es llença si existeix una activitat a la mateixa rutina que se solapa temporalment amb la creada
+     */
     public void createActivity(String routineName, String name, String description, String theme, int iniH, int iniM, int endH, int endM, String iniDayString, String endDayString) throws InvalidTimeIntervalException, InvalidDayIntervalException, InvalidTimeException, OverlappingActivitiesException {
         getInstanceActivityAdapter();
         getInstanceRoutineAdapter();
@@ -48,21 +95,21 @@ public class DomainAdapterFactory {
         int comparison = iniDay.compareTo(endDay); //negatiu si iniDay<endDay; 0 si iguals; positiu si iniDay>endDay
         if(comparison < 0){ //activitat en dies diferents
             //creacio activitat dia 1
-            Activity activity1 = activityAdapter.createActivity(name, description, theme, iniH, iniM, 23, 59, iniDay);
+            //Activity activity1 = activityAdapter.createActivity(name, description, theme, iniH, iniM, 23, 59, iniDay);
             //routineAdapter.addActivity(routineName, activity1);
             String beginTime = iniH + ":" + iniM;
             String endTime = "23:59";
             controllerPersistence.createActivity(routineName, name, theme, description, iniDayString, beginTime, endTime);
             //creacio activitat dia 2
-            Activity activity2 = activityAdapter.createActivity(name, description, theme, 0, 0, endH, endM, endDay);
+            //Activity activity2 = activityAdapter.createActivity(name, description, theme, 0, 0, endH, endM, endDay);
             //routineAdapter.addActivity(routineName, activity2);
             beginTime = "00:00";
             endTime = endH + ":" + endM;
             controllerPersistence.createActivity(routineName, name, theme, description, iniDayString, beginTime, endTime);
         }
         else if(comparison == 0){
-            Activity activity = activityAdapter.createActivity(name, description, theme, iniH, iniM, endH, endM, iniDay);
-            routineAdapter.addActivity(routineName, activity);
+            //Activity activity = activityAdapter.createActivity(name, description, theme, iniH, iniM, endH, endM, iniDay);
+            //routineAdapter.addActivity(routineName, activity);
             String beginTime = iniH + ":" + endM;
             String endTime = endH + ":" + endM;
             controllerPersistence.createActivity(routineName, name, theme, description, iniDayString, beginTime, endTime);
