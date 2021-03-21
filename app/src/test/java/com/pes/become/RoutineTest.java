127eb879048917c3d842a9aadc85b7025c4244ec
mxrcfdez <marc.fernandez.banos@estudiantat.upc.edu>
2021-03-21 19:18:37 +0100

    routine test

--- app/src/test/java/com/pes/become/RoutineTest.java
+++ app/src/test/java/com/pes/become/RoutineTest.java
@@ -25,4 +25,16 @@ public class RoutineTest {
         assertEquals(a,res);
     }
 
+    @Test
+    public void updateActivity() throws InvalidTimeIntervalException, InvalidTimeException {
+        Routine r = new Routine("test");
+        Activity old = new Activity("testActivity",new Theme("Sport"),17,0,18,0, Day.Monday);
+        Activity updated = new Activity("testActivityUpdated","sampleDescription", new Theme("Entertainment"),18,30,19,30, Day.Tuesday);
+        ActivityKey ak = new ActivityKey("testActivity", "test", Day.Monday, new TimeInterval(17,0,18,0));
+        r.addActivity(old);
+        r.updateActivity(ak,"testActivityUpdated","sampleDescription",18,30,19,30, "Tuesday");
+        Activity res = r.getActivity(ak);
+        assertEquals(res,updated);
+    }
+
 }
