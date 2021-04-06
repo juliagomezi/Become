31db101af6ee1ceffa33c0ee55be80e7464c333e
Alvaro <alvaro.andres@estudiantat.upc.edu>
2021-04-06 20:52:09 +0200

    Login screen

---
+++ app/src/main/java/com/pes/become/frontend/login.java
@@ -0,0 +1,16 @@
+package com.pes.become.frontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pes.become.R;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
