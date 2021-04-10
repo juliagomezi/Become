9f3f892725a805771eec40d389be6ff03bc19d98
Ignasi <ignasi.de.jose@estudiantat.upc.edu>
2021-04-10 12:46:50 +0200

    Hem posat IDs a les rutines i quan es borra una rutina també es borren les activitats.

--- app/src/main/java/com/pes/become/backend/persistence/ControllerRoutineDB.java
+++ app/src/main/java/com/pes/become/backend/persistence/ControllerRoutineDB.java
@@ -1,9 +1,16 @@
 package com.pes.become.backend.persistence;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class ControllerRoutineDB {
 
@@ -11,29 +18,63 @@ public class ControllerRoutineDB {
     final FirebaseFirestore db;
 
     /**
-     * Creadora per defecte
+     * Creadora per defecte.
      */
     public ControllerRoutineDB() {
         db = FirebaseFirestore.getInstance();
     }
 
     /**
-     * Crear una rutina nova
-     * @param routineName nom de la rutina a crear
+     * Crear una rutina nova.
+     * @param routineName nom de la rutina a crear.
      */
-    public void createRoutine(String routineName) {
+    public String createRoutine(String routineName) {
         Map<String,String> dataInput = new HashMap<>();
         dataInput.put("name", routineName);
-        db.collection("routines").document(routineName).set(dataInput);
+        DocumentReference docRefToRoutine= db.collection("routines").document();
+        docRefToRoutine.set(dataInput);
+        return docRefToRoutine.getId();
     }
 
     /**
-     * Eliminar una rutina existent
-     * @param routineName nom de la rutina a eliminar
+     * Eliminar una rutina existent.
+     * @param idRoutine identificador de la rutina a eliminar.
      */
-    public void deleteRoutine(String routineName) {
-        db.collection("routines").document(routineName).delete();
+    public void deleteRoutine(String idRoutine) {
+        DocumentReference DocRefToRoutine = db.collection("routines").document(idRoutine);
+        CollectionReference colRefToActivities = DocRefToRoutine.collection("activities");
+
+        deleteActivities(colRefToActivities,10);
+
+
+        DocRefToRoutine.delete();
     }
+
+    /**
+     *
+     * @param collection és la referència a la col·lecció que es vol borrar.
+     * @param batchSize és el límit de documents que pot agafar.
+     */
+    private void deleteActivities(CollectionReference collection, int batchSize) {
+        try {
+            // retrieve a small batch of documents to avoid out-of-memory errors
+            Task<QuerySnapshot> future = collection.limit(batchSize).get();
+            int deleted = 0;
+            // future.get() blocks on document retrieval
+            List<DocumentSnapshot> documents = future.getResult().getDocuments();
+            for (DocumentSnapshot document : documents) {
+                document.getReference().delete();
+                ++deleted;
+            }
+            if (deleted >= batchSize) {
+                // retrieve and delete another batch
+                deleteActivities(collection, batchSize);
+            }
+        } catch (Exception e) {
+            System.err.println("Error deleting collection : " + e.getMessage());
+        }
+    }
+
 }
 
 
---
+++ app/src/main/java/com/pes/become/backend/persistence/CtrlUsuari.java
@@ -0,0 +1,87 @@
+package com.pes.become.backend.persistence;
+
+import android.app.Activity;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class CtrlUsuari {
+
+    private FirebaseAuth mAuth;
+
+
+
+    public void createAccount(String email, String password, Activity act,Method method, Object object){
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(act, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Object[] params = new Object[1];
+                            params[0] = user;
+
+                            try {
+                                method.invoke(object, params);
+                            } catch (IllegalAccessException e1) {
+                                System.out.println("Acces invàlid");
+                            } catch (InvocationTargetException e2) {
+                                System.out.println("Target no vàlid");
+                            }
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "createUserWithEmail:failure", task.getException());
+                            //Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
+    }
+
+
+
+
+        /*
+    //TUTORIAL
+    public void tutorial(String routineName, String day)  {
+
+        Class[] parameterTypes = new Class[1];
+        parameterTypes[0] = ArrayList.class;
+        Method method1 = ControllerPersistence.class.getMethod("dothingsWithActivity", parameterTypes);
+        this.getActivitiesByDay(routineName,day,method1,this);
+    }
+
+    public void dothingsWithActivity(ArrayList<String> message)
+    {
+        for(String a: message) Log.d("DoThingsWithActivity", a);
+    }
+    //FI TUTORIAL*/
+
+
+    public void onCreate(){
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+
+
+    public void onStart(){
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            //Retornar si l'usuari ha iniciat sessio o no
+        }
+    }
+
+}
