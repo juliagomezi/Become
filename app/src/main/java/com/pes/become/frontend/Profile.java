d121bb339756fcc8cf6aae6c2dbabf5e2363e2b2
mxrcfdez <marc.fernandez.banos@estudiantat.upc.edu>
2021-04-26 12:11:38 +0200

    fix permisos

--- app/src/main/java/com/pes/become/frontend/Profile.java
+++ app/src/main/java/com/pes/become/frontend/Profile.java
@@ -2,7 +2,9 @@ package com.pes.become.frontend;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -19,10 +21,11 @@ import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.provider.MediaStore;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -93,16 +96,6 @@ public class Profile extends Fragment {
         tabLayout.getTabAt(1).setIcon(R.drawable.stats_icon);
 
         return view;
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode==10) {
-            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                changeProfilePic(); // no s'obre la galeria quan li dones a permetre, has de tornar a clicar
-            }
-        }
     }
 
     /**
@@ -187,7 +180,24 @@ public class Profile extends Fragment {
             intent.setAction(Intent.ACTION_GET_CONTENT);
             startActivityForResult(intent, 1);
         } else {
-            ActivityCompat.requestPermissions((Activity) global,new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, 10);
+            requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, 10);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==10) {
+            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                changeProfilePic();
+            } else {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(global);
+                alertDialogBuilder.setTitle(R.string.permissionDeniedTitle);
+                alertDialogBuilder.setMessage(R.string.permissionDenied);
+                alertDialogBuilder.setPositiveButton("OK", (dialog, id) -> {});
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+            }
         }
     }
 
