1bffa57e87005c6ef84c9dc520477642a9dd1924
OriolFD <oriol.freixa.dachs@estudiantat.upc.edu>
2021-04-30 13:36:50 +0200

    Calenari enllestit

--- app/src/main/java/com/pes/become/backend/persistence/ControllerCalendarDB.java
+++ app/src/main/java/com/pes/become/backend/persistence/ControllerCalendarDB.java
@@ -4,9 +4,17 @@ import android.util.Log;
 
 import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -31,28 +39,154 @@ public class ControllerCalendarDB {
     /**
      * Crea un dia al calendari nou.
      * @param userId identificador de l'usuari
-     * @param dia dia a crear.
-     * @param routineId id de la rutina a la que referenciem
+     * @param day dia a crear.
+     * @param routineId id de la rutina a la que referencia
      * @param totalActivites nombre d'activitats totals del dia de la rutina que estem afegint.
      */
-    public String addDay(String userId, Date dia, String routineId, int totalActivites) {
+    public String addDay(String userId, Date day, String routineId, int totalActivites) {
         Log.w("Calendar", "Iniciant operacio addDay");
         Map<String,Object> dataInput = new HashMap<>();
         dataInput.put("idRoutine", routineId);
         dataInput.put("numActivitiesDone", 0);
         dataInput.put("numTotalActivities", totalActivites);
-        //Operacions calendari
-        Calendar calendar = GregorianCalendar.getInstance();
-        calendar.setTime(dia);
-        if(calendar.get(calendar.HOUR_OF_DAY) != 0) calendar.set(calendar.HOUR_OF_DAY, 0);
-        if(calendar.get(Calendar.MINUTE) != 0) calendar.set(calendar.MINUTE, 0);
-        if(calendar.get(Calendar.SECOND) != 0) calendar.set(calendar.SECOND, 0);
-        if(calendar.get(Calendar.MILLISECOND) != 0) calendar.set(calendar.MILLISECOND, 0);
-        dataInput.put("day", new Timestamp(calendar.getTime()));
+        dataInput.put("day", getStringDay(day));
+        dataInput.put("month", getStringMonth(day));
+        dataInput.put("year", getStringYear(day));
 
         Log.w("Calendar", "A punt de crear un nou dia");
-        DocumentReference docRefToDay= db.collection("users").document(userId).collection("calendar").document();
+        DocumentReference docRefToDay= db.collection("users").document(userId).collection("calendar").document(getStringDate(day));
         docRefToDay.set(dataInput);
         return docRefToDay.getId();
+    }
+
+    /**
+     * Actualitza la informació d'un dia
+     * @param userId id de l'usuari del calendari
+     * @param day dia del calendari
+     * @param activitiesDone nou nombre d'activitats fetes (a -1 no actualitzarà res)
+     * @param idRoutine nova id de la rutina a la que referencia
+     */
+    public void updateDay(String userId, Date day, int activitiesDone, String idRoutine){
+        DocumentReference docRefToRoutine = db.collection("users").document(userId).collection("calendar").document(getStringDate(day));
+        if(activitiesDone >= 0)docRefToRoutine.update("numActivitiesDone", activitiesDone);
+        if(idRoutine != null && idRoutine != "")docRefToRoutine.update("idRoutine", idRoutine);
+    }
+    /**
+     * Executa el metode method amb un hashmap que representa el day de la base de dades si aquest s'ha pogut consultar, o l'excepció que ha saltat si no.
+     * Day tindrà les claus: day, idRoutine, numActivitiesDone, numTotalActivities. Totes son strings
+     * @param userId id de l'usuari del calendari
+     * @param day dia del calendari
+     * @param method metode a executar
+     * @param object objecte del metode a executar
+     */
+    public void getDay(String userId, Date day, Method method, Object object){
+
+        db.collection("users").document(userId).collection("calendar").document(getStringDate(day))
+            .get().addOnCompleteListener(task -> {
+            Object[] params = new Object[2];
+                params[0] = task.isSuccessful();
+                if (task.isSuccessful()) {
+
+                     DocumentSnapshot document = task.getResult();
+                     if(document.exists()) {
+                         HashMap<String, String> hashAux = new HashMap<String, String>();
+                         hashAux.put("day", document.get("day").toString());
+                         hashAux.put("month", document.get("month").toString());
+                         hashAux.put("year", document.get("year").toString());
+                         hashAux.put("idRoutine", document.get("idRoutine").toString());
+                         hashAux.put("numActivitiesDone", document.get("numActivitiesDone").toString());
+                         hashAux.put("numTotalActivities", document.get("numTotalActivities").toString());
+                         params[1] = hashAux;
+                     }
+                     else
+                     {
+                         params[0] = false;
+                         params[1] = new Exception("Tried to get a day that didn't exist");
+                     }
+                }else params[1] = task.getException();
+                try {
+                    method.invoke(object, params);
+                } catch (IllegalAccessException ignore) {
+                } catch (InvocationTargetException ignore) {
+                }
+        });
+    }
+
+    /**
+     * Retorna els dies de la base de dades del mes indicat
+     * @param userId id de l'usuari del calendari
+     * @param month més que volem seleccionar
+     * @param method metode a executar
+     * @param object objecte del metode a executar
+     */
+    public void getAvailableDays(String userId, int month, Method method, Object object)
+    {
+        db.collection("users").document(userId).collection("calendar").whereEqualTo("month", String.format("%02d", month))
+                .get().addOnCompleteListener(task -> {
+            Object[] params = new Object[2];
+            params[0] = task.isSuccessful();
+            if (task.isSuccessful()) {
+                ArrayList<HashMap<String,String>> list = new  ArrayList<HashMap<String,String>>();
+                QuerySnapshot documents = task.getResult();
+                for(DocumentSnapshot document: documents) {
+                    HashMap<String, String> hashAux = new HashMap<String, String>();
+                    hashAux.put("day", document.get("day").toString());
+                    hashAux.put("month", document.get("month").toString());
+                    hashAux.put("year", document.get("year").toString());
+                    hashAux.put("idRoutine", document.get("idRoutine").toString());
+                    hashAux.put("numActivitiesDone", document.get("numActivitiesDone").toString());
+                    hashAux.put("numTotalActivities", document.get("numTotalActivities").toString());
+                    list.add(hashAux);
+                }
+                params[1] = list;
+
+            }else params[1] = task.getException();
+            try {
+                method.invoke(object, params);
+            } catch (IllegalAccessException ignore) {
+            } catch (InvocationTargetException ignore) {
+            }
+        });
+    }
+
+    /**
+     * Retorna la data en format per la ID de la base de dades
+     * @param date data
+     * @return
+     */
+    private String getStringDate(Date date)
+    {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        return dateFormat.format(date);
+    }
+    /**
+     * Retorna la data en format per el dia de la base de dades
+     * @param date data
+     * @return
+     */
+    private String getStringDay(Date date)
+    {
+        DateFormat dateFormat = new SimpleDateFormat("dd");
+        return dateFormat.format(date);
+    }
+    /**
+     * Retorna la data en format pel mes de la base de dades
+     * @param date data
+     * @return
+     */
+    private String getStringMonth(Date date)
+    {
+        DateFormat dateFormat = new SimpleDateFormat("MM");
+        return dateFormat.format(date);
+    }
+    /**
+     * Retorna la data en format per l'any de la base de dades
+     * @param date data
+     * @return
+     */
+    private String getStringYear(Date date)
+    {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy");
+        return dateFormat.format(date);
     }
 }
