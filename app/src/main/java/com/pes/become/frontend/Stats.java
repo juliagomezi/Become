a4b75012c3de5d97fc7e1e03aa3c70cf0fb6d90f
byAlcon <alvaroandres4@gmail.com>
2021-05-01 16:00:21 +0200

    line chart

--- app/src/main/java/com/pes/become/frontend/Stats.java
+++ app/src/main/java/com/pes/become/frontend/Stats.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
@@ -128,17 +129,6 @@ public class Stats extends Fragment {
     {
         selectedDate = selectedDate.plusMonths(1);
         setMonthView();
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    @Override
-    public void onItemClick(int position, String dayText)
-    {
-        if(!dayText.equals(""))
-        {
-            String message = "Selected Date " + dayText + " " + monthYearFromDate(selectedDate);
-            Toast.makeText(global, message, Toast.LENGTH_LONG).show();
-        }
     }
 
     private void setHoursStats() {
